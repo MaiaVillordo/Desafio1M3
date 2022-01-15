e07f8d157d50e0e816af37d19328b588ebda95c9
MaiaVillordo <maiaunne@gmail.com>
2022-01-14 22:56:56 -0300

    Desafíos

---
+++ g14_VillordoMaia_D1.py.py
@@ -0,0 +1,7 @@
+# Programa que ingresa un nombre y una edad y devuelve si puede votar o no
+nombre = input("Ingrese un nombre ")
+edad = input("Ingrese una edad  ")
+if edad >= "16" :
+    print (nombre + " puede votar")
+else:
+    print (nombre + " debe esperar para votar") 
